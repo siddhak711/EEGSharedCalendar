94c73b0b2903008f09dbf23d27a03806c9e809f5
Siddha Kanthi <siddhakanthi@gmail.com>
2025-11-19 17:36:44 -0500

    Posthog finally working

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -1,20 +1,27 @@
-"use client"
+'use client'
+
+import { usePathname, useSearchParams } from "next/navigation"
 
 import { useEffect } from "react"
-import posthog from "posthog-js"
-import { PostHogProvider as PHProvider } from "posthog-js/react"
+
+import { usePostHog } from 'posthog-js/react'
+
+import posthog from 'posthog-js'
+
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-        defaults: '2025-05-24',
-        capture_exceptions: true, // This enables capturing exceptions using Error Tracking
-        debug: process.env.NODE_ENV === "development",
-      })
-    }
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      defaults: '2025-05-24'
+    })
   }, [])
 
-  return <PHProvider client={posthog}>{children}</PHProvider>
+  return (
+    <PHProvider client={posthog}>
+      {children}
+    </PHProvider>
+  )
 }
